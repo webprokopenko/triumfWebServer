68a3293e31ff0a90de0af50aa6ff564e90b5ebed
Mykola <mykola_borodyn@ecoengineer.in.ua>
2018-05-08 11:05:58 +0300

    Auth refactoring

--- routes/api/v1.0/web/index.js
+++ routes/api/v1.0/web/index.js
@@ -4,9 +4,11 @@ const app = module.exports = express();
 const oauthController = require(`${appRoot}/controllers/oauthController`);
 const Auth = require(appRoot + '/lib/auth');
 
-app.post('/auth', Auth.autorize);
+app.post('/auth', function(req, res) {
+    Auth.authorize(req, res);
+});
 app.get('/', Auth.authen, function (req, res) {
-	res.send('Congratulations, you are in a secret area!');
+	res.send('Congratulations, you are in a secret area! Welcome ' + req.user.email);
 });
 
 app.get('/deffault', (req, res, next) => {
