41837c7647c1bbe4bb835d7178131dbeb5d14698
Prokopenko <web.prokopenko@gmail.com>
2018-05-11 09:46:31 +0300

    Auth part check

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,22 +2,16 @@ const intel = require('intel');
 const AuthError = intel.getLogger('AuthError');
 AuthError.setLevel(AuthError.ERROR).addHandler(new intel.handlers.File(`${appRoot}/logs/user/error.log`));
 
-const userModel = require(`${appRoot}/mongo/model/user`);
 const authLib = require(`${appRoot}/lib/auth`);
 
 const createUser = (data) => {
     return new Promise((resolve, reject) => {
         try {
-            userModel.create({
-                email: data.email,
-                password: data.password
-            }, (err, user) => {
-                if (err) {
-                    AuthError.error('createUser error: ' + err);
+            authLib._createUser(data)
+                .then(user => resolve(user))
+                .catch(err => {
                     return reject(err.code === 11000 ? 'User exists' : err);
-                }
-                return resolve(user);
-            });
+                })
         } catch (error) {
             AuthError.error('Error createUser authController' + error);
             return reject('Service error');
@@ -26,14 +20,21 @@ const createUser = (data) => {
 }
 const authorizeUser = (email, password) => {
     return new Promise((resolve, reject) => {
-        _authorization(email, password)
-            .then(token => {
-                (token ? resolve({ token: token }) : reject('Email password not exist'));
-            })
-            .catch(err => {
-                AuthError.error('authorizeUser error: ' + err);
-                return reject('Server error');
-            });
+        try {
+            authLib._findUserByEmail(email)
+                .then(user => {
+                    if (!user || user.password !== password) {
+                        return reject('Email password not exist');
+                    }
+                    authLib._getJWT(user._id.toString())
+                        .then(token => {
+                            return resolve({ token: token })
+                        })
+                })
+        } catch (err) {
+            AuthError.error('Error authorizeUser authController' + err);
+            return reject('Server error');
+        }
     });
 }
 
@@ -41,33 +42,20 @@ const authen = (req, res, next) => {
     return new Promise((resolve, reject) => {
         authLib._verifyJWT(req.headers['a-token'])
             .then((id) => {
-                userModel.findOne({ _id: id })
+                authLib._findUserById(id)
                     .then(user => {
                         req.user = user;
                         next();
                     })
                     .catch(err => {
                         AuthError.error('authen error: ' + err);
-                        res.statuse(401).send('');
+                        res.statuse(401).send('Service error');
                     })
             })
-            .catch(err => {
-                res.status(401).send('_verifyJWT error: ' + err);
+            .catch(() => {
+                res.status(401).send('JWT not verified');
             });
     });
-};
-
-const _authorization = async function (email, password) {
-    try{
-        const user = await userModel.findOne({ email: email });
-        if (!user || user.password !== password){
-            return false;
-        } 
-        const JWT =  await authLib._getJWT(user._id.toString());
-        return JWT;
-    } catch (err) {
-        console.log('_authorization error' + err);
-    }
 };
 
 module.exports = {
